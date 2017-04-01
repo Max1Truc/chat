665e95570ede1f431d30151fa319adf2db371b84
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-01 10:53:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var port = process.env.PORT || settings["port"];
 // load index.html
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+}).get('/alphabet', function (req, res) {
+  res.sendfile(__dirname + '/alphabet.txt');
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
@@ -21,9 +23,21 @@ io.sockets.on('connection', function (socket, pseudo) {
     });
 
     socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+		// test message.length
+		if (message.length < 2) {
+			socket.emit("message", { pseudo:"SERVER", message:"message too short"});
+		} else if (message.length =< 50) {
+			// Send
+			console.log("message from " + socket.pseudo + " : " + message);
+			message = ent.encode(message);
+			socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+			socket.emit('message', {pseudo: socket.pseudo, message: message});
+		} else {
+			socket.emit("message", { pseudo:"SERVER", message:"message too long"});
+		}
     }); 
 });
 
 server.listen(port);
+
+console.log("Server listening on port " + port)
